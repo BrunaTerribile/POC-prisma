ce046daf4f34295a7d5761cd7324b8e107a40cb9
Bruna Terribile <brunaterribile@gmail.com>
2023-01-27 11:23:41 -0300

    git clone ts repository

---
+++ src/repositories/pedals-repository.ts
@@ -0,0 +1,35 @@
+import { connection } from "../database/database.js";
+import Pedal from "../protocols/pedal.js";
+import { QueryResult } from "pg";
+
+async function getAll(): Promise<QueryResult<any>>{
+    return connection.query(
+        `SELECT * FROM pedals`
+    );
+}
+
+async function getById(id: number): Promise<QueryResult<any>>{
+    return connection.query(
+        `SELECT * FROM pedals
+        WHERE id = $1`,
+        [id]
+    );
+}
+
+async function addPedal(pedal: Pedal): Promise<QueryResult>{
+    return connection.query(
+        `INSERT INTO pedals (model, brand, value) 
+        VALUES ($1, $2, $3)
+        RETURNING id`,
+        [pedal.model, pedal.brand, pedal.value]
+    );
+}
+
+
+const pedalRepository = {
+    getAll,
+    getById,
+    addPedal
+}
+
+export default pedalRepository;
---
+++ src/repositories/sales-repository.ts
@@ -0,0 +1,64 @@
+import { connection } from "../database/database.js";
+import { QueryResult } from "pg";
+
+async function getAll(): Promise<QueryResult<any>>{
+    return connection.query(
+        `SELECT * FROM sales`
+    );
+}
+
+async function addSale(id: number, customer: string): Promise<QueryResult>{
+    return connection.query(
+        `INSERT INTO sales (pedal_id, customer) 
+        VALUES ($1, $2)`,
+        [id, customer]
+    );
+}
+
+async function getRank(): Promise<QueryResult<any>>{
+    return connection.query(
+        `SELECT p.brand, p.model, 
+        COUNT(s.pedal_id) as "Total Sales", 
+        SUM(p.value) as "Total USD" 
+        FROM pedals AS p
+        JOIN sales AS s
+        ON p.id = s.pedal_id
+        GROUP BY p.id
+        ORDER BY "Total Sales" DESC;`
+    );
+}
+
+async function addStock(id: number, quantity: number): Promise<QueryResult>{
+    return connection.query(
+        `INSERT INTO stock (pedal_id, quantity)
+        VALUES ($1, $2)`,
+        [id, quantity]
+    )
+}
+
+async function updateStock(id: number): Promise<QueryResult>{
+    return connection.query(
+        `UPDATE stock SET quantity = quantity -1
+        WHERE pedal_id = $1`,
+        [id]
+    )
+}
+
+async function cancelSale(id: string | string[]): Promise<QueryResult>{
+    return connection.query(
+        `DELETE FROM sales
+        WHERE id = $1`,
+        [id]
+    )
+}
+
+const saleRepository = {
+    getAll,
+    addSale,
+    getRank,
+    addStock,
+    updateStock,
+    cancelSale
+}
+
+export default saleRepository;
